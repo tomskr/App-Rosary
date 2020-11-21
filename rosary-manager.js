3a6b5149592915ac3dd7532ceada41b6387524ed
TomaszSkrzypek <tomskr3.ts@gmail.com>
2020-11-21 15:02:16 +0100

    Add ejs and body-parser

--- rosary-manager.js
+++ rosary-manager.js
@@ -1,15 +1,27 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+// set port
 const port = 3000
+// global variables
+let rosaryGroups = ["Rosary1","Rosary2"]
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', function (req, res) {
-  res.send('bey World')
+  res.render("index",{gropList: rosaryGroups})
 })
 
-app.get('/hobbies', function (req, res){
-  res.send('<ul><li>Coffes</li><li>Programing</li></ul>')
+app.post('/', function(req,res){
+  let item = req.body.groupName
+  rosaryGroups.push(item);
+  res.redirect('/')
 })
+
+
 
 app.listen(3000, function(){
-  console.log('Server connected to port ${port}')
+  console.log('Server connected to port ' + port)
 })
