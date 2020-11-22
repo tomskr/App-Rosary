393d585d9e65a7febeccc3167e7400937d7e93c7
TomaszSkrzypek <tomskr3.ts@gmail.com>
2020-11-22 17:29:23 +0100

    Added mongoose

--- rosary-manager.js
+++ rosary-manager.js
@@ -1,14 +1,51 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require("mongoose")
+
 const app = express()
+
 // set port
 const port = 3000
-// global variables
-let rosaryGroups = ["Rosary1","Rosary2"]
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static("public"))
+
+mongoose.connect("mongodb://localhost:27017/rosaryDB",{ useNewUrlParser: true })
+
+const rosaryGroupSchema = {
+  index: Number,
+  name: String,
+};
+
+const RosaryGroup = mongoose.model("RosaryGroup",rosaryGroupSchema)
+
+const group1 = new RosaryGroup({
+  index: 1,
+  name: "Teresa"
+})
+
+const group2 = new RosaryGroup({
+  index: 2,
+  name: "Antoni"
+})
+
+const group3 = new RosaryGroup({
+  index: 3,
+  name: "Maria"
+})
+
+const defaultRosaryGroups = [group1, group2, group3]
+
+RosaryGroup.insertMany(defaultRosaryGroups, function(err){
+  if(err){
+    console.log(err)
+  }else{
+    console.log("Success")
+  }
+})
 
 app.get('/', function (req, res) {
   res.render("index",{gropList: rosaryGroups})
