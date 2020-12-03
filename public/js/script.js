c325e6ec68622ce752ea11970d18d4485e1492ea
tomskr <tomskr3.ts@gmail.com>
2020-12-03 13:26:16 +0100

    single editmode partial

--- public/js/script.js
+++ public/js/script.js
@@ -12,5 +12,10 @@ $(function() {
      $('.toggle-hid').val(this.checked ? 1 : 0)
  })
 
-
-
+const divedit = document.getElementsByClassName("editmode")
+ if(divedit[0].innerText === "single-edit-mode"){
+    var ClassBtnEdit = document.getElementsByClassName("edit-blt");
+    for (let item of ClassBtnEdit) {
+      item.className = item.className + " edit-disable-btn"
+  }
+}
--- rosary-manager.js
+++ rosary-manager.js
@@ -7,7 +7,7 @@ const rosaryGroup = require('./models/rosaryGroup')
 const rosaryMember = require('./models/rosaryMember')
 const rosaryEvent = require('./models/rosaryEvent');
 const event = require('./event.js');
-
+const { json } = require('body-parser')
 
 const app = express()
 
@@ -18,7 +18,10 @@ const port = 3000
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}))
-app.use(express.static("public"))
+app.use(express.static(__dirname + '/public'));
+
+
+
 
 //mongoose
 mongoose.connect("mongodb://localhost:27017/rosaryDB",{ 
@@ -39,7 +42,9 @@ mongoose.connect("mongodb://localhost:27017/rosaryDB",{
 //grup/
 app.get(['/group','/'], async (req, res) => {
     const groupList = await rosaryGroup.find({}).sort({"order":1})
-    res.render('./pages/grupy.ejs',{groupList})
+    const editmode = ['edit-mode-on',null]
+    //temp
+    res.render('./pages/grupy.ejs',{groupList,editmode})
 })
 
 //group/new
@@ -52,10 +57,18 @@ app.post('/group', async (req,res) =>{
   })
   newGroup.save()
   console.log(newGroup)
-  res.redirect('/')
+  res.redirect('/group')
 })
 
-//group/:id/edit
+//group/:id/edit (get)
+app.get('/group/:id/edit', async (req,res) =>{
+  const groupList = await rosaryGroup.find({}).sort({"order":1})
+  const editmode = ['single-edit-mode',req.params.id]
+  console.log('edit')
+  res.render('./pages/grupy.ejs',{groupList,editmode})
+})
+
+//group/:id/edit (post)
 
 //group/:id/up
 app.get('/group/:id/up', async (req, res) => {
